e41f00d801efeb4c7f021a4b10ddbc408f407bad
dt-dragway <jdiaz@nuevaesparta.gob.ve>
2026-01-04 22:21:23 -0400

    refactor: reescribe la lógica del reloj a JavaScript puro, implementando formato de 12 horas y nombres de fecha en español.

--- web/assets/vendor/js/clock.js
+++ web/assets/vendor/js/clock.js
@@ -1,76 +1,44 @@
-$(document).ready(function() {
-    var monthNames = [ lang.January, lang.February, lang.March, lang.April, lang.May, lang.June, lang.July, lang.August, lang.September, lang.October, lang.November, lang.December ];
-    var dayNames= [lang.Sunday, lang.Monday, lang.Tuesday, lang.Wednesday, lang.Thursday, lang.Friday, lang.Saturday]
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ampm = h >= 12 ? 'PM' : 'AM';
+
+    // Convertir a formato de 12 horas
+    h = h % 12;
+    h = h ? h : 12; // La hora '0' debe ser '12'
+
+    m = checkTime(m);
+    s = checkTime(s);
+
     var timeMin = document.getElementById("time-min")
     var timeSec = document.getElementById("time-sec")
     var timeHour = document.getElementById("time-hours")
     var timeDate = document.getElementById("time-date")
-    var newDate = new Date();
-    newDate.setDate(newDate.getDate());
-    var updateDate = function(){
-        // var clockDateFormat = `$DAYNAME $DAY $MONTHNAME $YEAR`
-        timeDate.innerHTML = dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear();
+
+    if (timeHour) {
+        timeHour.innerHTML = h;
     }
-    if($user.details.clock_date_format){
-        var clockDateFormat = `${$user.details.clock_date_format}`
-        updateDate = function(){
-            const newTimeString = clockDateFormat
-                .replaceAll('$DAYNAME',dayNames[newDate.getDay()])
-                .replaceAll('$DAY',newDate.getDate())
-                .replaceAll('$MONTHNAME',monthNames[newDate.getMonth()])
-                .replaceAll('$YEAR',newDate.getFullYear());
-            timeDate.innerHTML = newTimeString;
-        }
+    if (timeMin) {
+        timeMin.innerHTML = m;
     }
-    var second = function(theDate) {
-	   var seconds = theDate.getSeconds();
-	   timeSec.innerHTML=( seconds < 10 ? "0" : "" ) + seconds;
-	}
-    var minute = function(theDate) {
-    	var minutes = theDate.getMinutes();
-        timeMin.innerHTML=(( minutes < 10 ? "0" : "" ) + minutes);
+    if (timeSec) {
+        timeSec.innerHTML = s + ' ' + ampm; // Agregar AM/PM después de los segundos
     }
-    var hour = function(theDate) {
-        var hours = theDate.getHours();
-        hours = ( hours < 10 ? "0" : "" ) + hours;
-        if(timeHour.classList.contains('twentyfour') && hours > 12)hours = hours - 12;
-        timeHour.innerHTML = hours
+    if (timeDate) {
+        var days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        var dayName = days[today.getDay()];
+        var day = today.getDate();
+        var monthName = months[today.getMonth()];
+        var year = today.getFullYear();
+        timeDate.innerHTML = dayName + ' ' + day + ' ' + monthName + ' ' + year;
     }
-    function setAll(){
-        var theDate = new Date()
-        var currentMinute = theDate.getMinutes()
-        var currentHour = theDate.getHours()
-        var currentDay = theDate.getHours()
-        second(theDate)
-        if(lastMinute !== currentMinute){
-            minute(theDate)
-            lastMinute = currentMinute
-        }
-        if(lastHour !== currentHour){
-            hour(theDate)
-            lastHour = currentHour
-        }
-        if(lastDay !== currentDay){
-            updateDate()
-            lastDay = currentDay
-        }
-    }
-    setAll()
-    var lastHour = newDate.getHours()
-    var lastMinute = newDate.getMinutes()
-    var lastDay = newDate.getDay()
-    setInterval(function(){
-        setAll()
-    },1000);
-    setInterval(function(){
-        updateDate()
-    },1000 * 60 * 60);
-    updateDate()
-    document.getElementById("clock").onclick = function(){
-        timeHour.classList.toggle('twentyfour')
-        lastHour = null
-        setAll()
-        lastHour = newDate.getHours()
-        updateDate()
-    }
-});
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+startTime()
